f4021de14dc5be5d7028ef49b5e9baa354610930
shin <sars21@hanmail.net>
2024-03-29 10:54:55 +0900

    fix task

--- src/tasks.rs
+++ src/tasks.rs
@@ -1,8 +1,14 @@
-use chrono::{DateTime, Utc};
+use std::fs::OpenOptions;
+use chrono::{serde::ts_seconds, DateTime, Utc, Local};
+use serde::{Deserialize, Serialize};
+use std::io::{Result, Seek, SeekFrom};
+use std::path::PathBuf;
 
-#[derive(Debug)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct Task {
     pub text: String,
+
+    #[serde(with = "ts_seconds")]
     pub created_at: DateTime<Utc>,
 }
 
@@ -11,4 +17,25 @@ impl Task {
         let created_at: DateTime<Utc> = Utc::now();
         Task { text, created_at }
     }
+}
+
+pub fn add_task(journal_path: PathBuf, task: Task) -> Result<()> {
+    // open file
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(journal_path)?;
+
+    let mut tasks: Vec<Task> = match serde_json::from_reader(&file) {
+        Ok(tasks) => tasks,
+        Err(e) if e.is_eof() => Vec::new(),
+        Err(e) => Err(e)?,
+    };
+
+    file.seek(SeekFrom::Start(0))?;
+    tasks.push(task);
+    serde_json::to_writer(file, &tasks)?;
+
+    Ok(())
 }
