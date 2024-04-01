9683e73c0d4ecbe80accbe9c2c79108b8bae4bab
shin <sars21@hanmail.net>
2024-04-01 09:57:45 +0900

    done task 2

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,33 @@
 mod cli;
 mod tasks;
+
 use structopt::StructOpt;
-
-
 use cli::{Action::*, CommandLineArgs};
 use tasks::Task;
+use std::path::PathBuf;
+use anyhow;
 
-fn main() {
+fn find_default_journal_file() -> Option<PathBuf> {
+    home::home_dir().map(|mut path| {
+        path.push(".rusty-journal.json");
+        path
+    })
+}
+
+fn main() -> anyhow::Result<()> {
     let CommandLineArgs {
         action,
         journal_file,
     } = CommandLineArgs::from_args();
 
-    let journal_file = journal_file.expect("Failed to find journal file");
+    let journal_file = journal_file
+        .or_else(find_default_journal_file)
+        .expect("Failed to find journal file");
 
     match action {
         Add { text } => tasks::add_task(journal_file, Task::new(text)),
         List => tasks::list_tasks(journal_file),
         Done { position } => tasks::complete_task(journal_file, position),
-    }
-    .expect("Failed to perform action")
+    }?;
+    Ok(())
 }
