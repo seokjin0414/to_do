2bc83a3e389b7da888b4f581068b98856e3ade32
shin <sars21@hanmail.net>
2024-04-01 16:30:34 +0900

    add test sample

--- src/main.rs
+++ src/main.rs
@@ -31,3 +31,21 @@ fn main() -> anyhow::Result<()> {
     }?;
     Ok(())
 }
+
+// test sample
+/*$ cargo run -- -j test-journal.json add "buy milk"
+
+$ cargo run -- -j test-journal.json add "take the dog for a walk"
+
+$ cargo run -- -j test-journal.json add "water the plants"
+
+$ cargo run -- -j test-journal.json list
+1: buy milk                                           [2021-01-08 16:39]
+2: take the dog for a walk                            [2021-01-08 16:39]
+3: water the plants                                   [2021-01-08 16:39]
+
+$ cargo run -- -j test-journal.json done 2
+
+$ cargo run -- -j test-journal.json list
+1: buy milk                                           [2021-01-08 16:39]
+2: water the plants                                   [2021-01-08 16:39]*/
